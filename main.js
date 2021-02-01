4211bc4f4b1c50e2b88312ac7eaf10dd369a9beb
Cymiee <71879391+Cymiee@users.noreply.github.com>
2021-02-01 13:42:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const prefix = '-';
+
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/.').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
+client.once('ready', () => {
+    console.log('Sleeperz Bot is online!');
+});
+
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'ping') {
+        client.commands.get('ping').execute(message, args);
+    } else if (command === 'youtube') {
+        client.commands.get('youtube').execute(message, args);
+    } else if (command === 'kick') {
+        client.commands.get('kick').execute(message, args);
+    } else if (command === 'ban') {
+        client.commands.get('ban').execute(message, args);
+    } else if (command === 'mute') {
+        client.commands.get('mute').execute(message, args);
+    } else if (command === 'unmute') {
+        client.commands.get('unmute').execute(message, args);
+    } else if (command === 'marco') {
+        client.commands.get('marco').execute(message, args);
+    }
+});
+
+
+client.login('NzkwODE4ODM5MzcyNzU5MDgw.X-GJcA.7Y4DQEMAkt7XInZV8uNMhh4FzOU');
